7567409a279e8e1e424e8cbca99a7a19ee5cf326
Ashish Kumar <ashish380431@gmail.com>
2025-01-12 23:38:36 +0530

    arr, set, dict - concepts added

---
+++ learn_tuple.py
@@ -0,0 +1,41 @@
+# TUPLES
+
+# coordinate1 = (1,2,3)
+# coordinate2 = (2,3,4)
+# print(coordinate1, coordinate2)
+# sum_of_coordinates = coordinate1 + coordinate2
+# print(sum_of_coordinates)
+
+
+# for i in coordinate1:
+#     print(i)
+# for i in range(len(coordinate1)):
+#     print(coordinate1[i])
+
+# print(coordinate1.index(1))
+
+
+
+# DICTIONARY
+
+init_dict = {}
+#print(init_dict['name']) #key error
+
+# print(init_dict.get('name','ashish'))
+# init_dict['name'] = 89
+# init_dict['age'] = 23
+# init_dict['balance'] = 1000
+# init_dict.pop('balance')
+# print(init_dict)
+
+
+
+# SETS
+# init_set = {1,2,3,4,5,6}
+# set2 = {4,5,10}
+# print(init_set, type(init_set))
+# print(init_set | set2) #union
+# print(init_set & set2) #intersection
+# print(init_set - set2) #difference
+# print(init_set ^ set2) #symmetric difference
+
