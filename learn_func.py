5e042695744aca80a8a9614e523235714e076d22
Ashish Kumar <ashish380431@gmail.com>
2025-01-12 23:51:41 +0530

    func initited

---
+++ learn_func.py
@@ -0,0 +1,29 @@
+# anonymous function
+# add = lambda a,b:a+b
+# myfunc = lambda a,b,c:(2*(a+b+c))+(a**2)+(b**2)+(c**2)
+# print(add(1,2))
+# print(myfunc(1,2,3))
+# def sum_arr(*arr):
+#     sum = 0
+#     for i in arr:
+#         sum+=i
+#     return sum
+
+# print(sum_arr(1,2,3,4,5,6,7,8,9,10))
+
+
+# def func(a,b,c):
+#     return a+b+c
+
+# print(func(*(1,2,3)))
+# print(func(*[1,2,3]))
+
+
+# def object_key_val_print(**obj):
+#     for key,value in obj.items():
+#         print(key,value)
+
+# print(object_key_val_print(name='ashish',age=23,balance=1000))
+# print(object_key_val_print(**{'name':'ashish','age':23,'balance':1000}))
+
+
